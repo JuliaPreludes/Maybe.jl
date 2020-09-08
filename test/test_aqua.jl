a25f89a149156339d3ec90989a6f1e561c1339c0
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-07 23:37:43 -0700

    Run optional tests in Aqua.test_all

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -3,6 +3,12 @@ module TestAqua
 using Aqua
 using Maybe
 
-Aqua.test_all(Maybe)
+Aqua.test_all(
+    Maybe;
+    project_extras = true,
+    stale_deps = true,
+    deps_compat = true,
+    project_toml_formatting = true,
+)
 
 end  # module
