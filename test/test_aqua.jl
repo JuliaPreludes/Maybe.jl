c1339baa05a9803fb5bce29b8b29fd4f7e805304
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-07 23:57:19 -0700

    Fix test/test_aqua.jl

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,13 +1,13 @@
 module TestAqua
 
 using Aqua
-using Compat
 using Maybe
+using Test
 
 Aqua.test_all(
     Maybe;
     project_extras = true,
-    stale_deps = (; ignore = [Compat]),
+    stale_deps = (; ignore = [:Compat]),
     deps_compat = true,
     project_toml_formatting = true,
 )
