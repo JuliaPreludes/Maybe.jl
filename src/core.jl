cfa5d1baf95c949acb9e6b15d2c89151e12b06f2
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-26 19:10:05 -0700

    Fix Maybe.first

--- src/core.jl
+++ src/core.jl
@@ -6,7 +6,7 @@ Maybe.length(itr) =
 Maybe.eltype(itr) =
     Base.IteratorEltype(itr) isa Base.HasEltype ? Some(eltype(itr)) : nothing
 
-@inline Maybe.first(xs) = @something(iterate(xs), return)[1]
+@inline Maybe.first(xs) = Some(@something(iterate(xs), return)[1])
 @inline Maybe.last(xs) = Maybe.get(xs, lastindex(xs))
 
 @inline function Maybe.get(x::Union{AbstractDict,NamedTuple}, k)
